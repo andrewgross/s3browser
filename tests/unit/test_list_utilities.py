3744edc398039d44f946f7656eff9e21563822cb
Andrew Gross <andrew.w.gross@gmail.com>
2016-06-27 17:30:46 -0400

    Adds tests for get_matches, shows uncovered lines

--- tests/unit/test_list_utilities.py
+++ tests/unit/test_list_utilities.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from s3browser.list_utilities import (
-    sort_files
+    sort_files,
+    get_names,
+    get_matches
 )
 from tests.util import get_unsorted_list_of_files
 
@@ -19,3 +21,31 @@ def test_sort_files():
 
     # Then I have sorted files
     map(lambda x: x.name, _sorted).should.equal(["a", "b", "c"])
+
+
+def test_get_names():
+    """
+    Get names from file objects
+    """
+    # When I have an unsorted list of files
+    files = get_unsorted_list_of_files()
+
+    # And when I get the names
+    names = get_names(files)
+
+    # Then I have just names
+    set(names).should.equal(set(("a", "b", "c")))
+
+
+def test_get_matches():
+    """
+    Filter files to a list of matches
+    """
+    # When I have a list of files
+    files = get_unsorted_list_of_files()
+
+    # And when I get the matches
+    matches = get_matches("a", files)
+
+    # Then I have just that match
+    map(lambda x: x.name, matches).should.equal(["a"])
